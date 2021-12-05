49fce7453f8183143857f022fda59ff0777211ef
Kishore P <pskishore.aug@gmail.com>
2021-12-04 23:52:07 -0500

    created basic flappy bird game. Only collision with floor works.

--- main.cpp
+++ main.cpp
@@ -128,33 +128,220 @@ int Rules::touchedButton(float touched_x, float touched_y){
  */
 class Play{
     public:
-        Play(Button *bk);
+        Play(Button *bk, Button *rtry);
         void draw();
         int touchedButton(float touched_x, float touched_y);
+        void drawPipes(int x, int top_y, int width);
+        void clearPipes(int x, int top_y, int width);
+        void movePipes();
+        void drawFlappy(float x, float old_y, float y);
+        void displayScore();
+        void retryMenu();
+        // sets the value of "hit"
+        void collided();
     private:
-        Button *back;
+        Button *backBTN;
+        Button *retryBTN;
+        int scoreCounter = 0;
+        float initial_y, final_y;
+        float initial_v, final_v;
+        // hit = true means flappy hit a pipe or floor. hit = false means flappy hasn't hit anything
+        bool hit = false;
 };
 
-Play::Play(Button *bk){
-    back = bk;
+// constructs the Play class with play and retry buttons
+Play::Play(Button *bk, Button *rtry){
+    backBTN = bk;
+    retryBTN = rtry;
 }
 
-// Arad: Work on formatting Rules
+// NOT COMPLETE
 void Play::draw(){
     LCD.Clear();
-    (*back).draw();
-    LCD.WriteAt("Game here.",0,0);
+
+    // Drawing the ground
+    LCD.SetFontColor(LIGHTGREEN);
+    LCD.FillRectangle(0,220,319,20);
+
+    
+    // setting initial y location to middle of screen
+    initial_y = 110;
+    // setting initial y velocity and final velocity to 0
+    initial_v = 0;
+    final_v = 0;
+
+    // drawing flappy at initial y.
+    drawFlappy(130,initial_y,initial_y);
+
+    LCD.SetFontColor(WHITE);
+    LCD.WriteAt("click to start", 120, 150);
+    int arb_x, arb_y;
+    // waiting for user input 
+     while(!LCD.Touch(&arb_x, &arb_y)){
+        // wait until screen is not being touched
+    }
+    while(LCD.Touch(&arb_x, &arb_y)){
+        // wait until person lets go
+    }
+
+    LCD.SetFontColor(BLACK);
+    LCD.FillRectangle(130,0,320,220);
+
+    // moving the pipes constantly to the left till flappy hasn't "hit" anything
+    while(!hit){
+        movePipes();
+    }
+
+    retryMenu();
+
+    // resetting collision to false and counter to 0
+    hit = false;
+    scoreCounter = 0;
+
+    std::cout << hit;
+    LCD.SetFontColor(WHITE);
 }
 
-// touching the back button in Rules returns 1. Else, returns 0.
+
+
+// displays the pipes' left end at given x
+void Play::drawPipes(int x, int top_y, int width){
+    LCD.SetFontColor(GREEN);
+    LCD.FillRectangle(x,top_y,width,220-top_y);
+    LCD.FillRectangle(x,0,width,top_y-66);
+} 
+
+// clears the previously drawn pipes located at x
+void Play::clearPipes(int x, int top_y, int width){
+    LCD.SetFontColor(BLACK);
+    LCD.FillRectangle(x,top_y,width,220-top_y);
+    LCD.FillRectangle(x,0,width,top_y-66);
+}
+
+// moves the given pipe from x = 320 to x = -40, while avoiding wrapping. Also increments the score counter
+void Play::movePipes(){
+    int x = 320;
+    int top_y = rand() % (210 + 1 - 76) + 76;
+
+    // repeat till the pipes are completely off the screen and the flappy bird hasn't hit anything.
+    while(x >= -40 && !hit){
+
+        // display score
+        displayScore();
+
+        // arbitrary x and y 
+        float arb_x; 
+        float arb_y;
+
+        if(LCD.Touch(&arb_x, &arb_y)){
+            // set the bird going up
+            final_v = 4;
+        } else { 
+            // increase velocity of the bird downward
+            final_v = initial_v - 0.8;
+            // terminal velocity
+            if(final_v <= -2){
+                final_v = -4;
+            }
+        }
+
+        float distanceFlappyMoved = (initial_v + final_v)/2;
+        final_y = initial_y - distanceFlappyMoved;
+        drawFlappy(160,initial_y, final_y);
+        // updating the value of "hit".
+        collided();
+
+        initial_v = final_v;
+        initial_y = final_y; 
+
+        // removes wrapping of the pipe at x < 0
+        if(x < 0){
+            drawPipes(0, top_y, 30 + x);
+            Sleep(3);
+            clearPipes(0,top_y, 30 + x);
+        } 
+        // removes wrapping of the pipe at x > 290
+        else if(x > 290){
+            drawPipes(x, top_y, 320 - x);
+            Sleep(3);
+            clearPipes(x,top_y, 320 - x);
+        }
+        // displays the pipe when x => 0 and x <= 290
+        else {
+            drawPipes(x, top_y, 30);
+            Sleep(3);
+            clearPipes(x,top_y, 30);
+        }
+
+        // incrementing counter if pipe pass center of screens
+        if(x == 130){
+            scoreCounter++;
+        }
+
+        // moves pipes back one
+        x -= 2;
+    }
+}
+
+// clears flappy bird at (x,old_y) and draws a flappy character at (x,y)
+void Play::drawFlappy(float x, float old_y, float y){
+
+    // Drawing flappy if the flappy is within bounds of LCD display
+    if(y > -13){
+        LCD.SetFontColor(BLACK);
+        LCD.FillCircle(x,old_y,8);
+        LCD.SetFontColor(YELLOW);
+        LCD.FillCircle(x,y,8);
+
+        // Drawing the ground to prevent marks on ground.
+        LCD.SetFontColor(LIGHTGREEN);
+        LCD.FillRectangle(0,220,319,20);
+
+        // drawing portions flappy at the upper portion of the LCD, if the flappy
+        // is partly visible in the screen.
+        if(y < 8){
+            LCD.SetFontColor(LIGHTGREEN);
+            LCD.FillRectangle(152,y-8,20,abs(y-8));
+        }
+    }
+}
+
+// returns true if the flappy bird hit a pipe or the ground. [NOT DONE]
+void Play::collided(){
+    // hiting the floor
+    if(final_y >= 212){
+        hit = true;
+    }
+}
+
+// creates retry menu
+void Play::retryMenu(){
+    LCD.SetFontColor(WHITE);
+    LCD.WriteAt("Score:",120,45);
+    LCD.WriteAt(scoreCounter,144,80);
+    (*backBTN).draw();
+    (*retryBTN).draw();
+}
+
+// touching the back button in Play returns 1. Touching the retry button in Play returns 2. Else, returns 0.
 int Play::touchedButton(float touched_x, float touched_y){
     int touched = 0;
-    if((*back).touched(touched_x,touched_y)){
+    if((*backBTN).touched(touched_x,touched_y)){
         touched = 1;
+    } else if((*retryBTN).touched(touched_x, touched_y)){
+        touched = 2;
     }
     return touched;
 }
 
+
+void Play::displayScore(){
+        // clearing the previous score and setting new score
+        LCD.SetFontColor(BLACK);
+        LCD.FillRectangle(290,0,26,19);
+        LCD.SetFontColor(WHITE);
+        LCD.WriteAt(scoreCounter,290,0);
+}
 
 // Checks if a button was clicked on given "onScreen".
 bool touchedAnyButton(int x, int y, int *onScreen, Main_Menu *menu, Rules *rules, Play *play){
@@ -190,6 +377,12 @@ bool touchedAnyButton(int x, int y, int *onScreen, Main_Menu *menu, Rules *rules
             *onScreen = 1;
             touched = true;
         }
+        
+        // Retry button was touched in Play
+        if((*play).touchedButton(x,y) == 2){
+            *onScreen = 2;
+            touched = true;
+        }
     }
 
     // in Rules Screen
@@ -223,7 +416,7 @@ void initiate(int *onScreen, Main_Menu *menu, Rules *rules, Play *play)
         // the actions quit performs
         LCD.Clear();
         LCD.WriteLine("Game Terminated.");
-        Sleep(3.0);
+        Sleep(2.0);
         LCD.Clear();
         LCD.WriteLine("Bye");
         Sleep(1.0);
@@ -244,10 +437,12 @@ int main() {
     Button rulesBTN("RULES",170,120,5*12 + 2, 19);
     Button quitBTN("QUIT",130,150,4*12 + 2, 19);
     Button backBTN("<-",280,0,2*12 + 2, 19);
+    Button playBackBTN("<-",100,120,2*12 + 2, 19);
+    Button playRetryBTN("RETRY",180,120,5*12 + 2, 19);
 
     Main_Menu myMenu(&playBTN, &rulesBTN, &quitBTN);
     Rules myRules(&backBTN);
-    Play myPlay(&backBTN);
+    Play myPlay(&playBackBTN,&playRetryBTN);
 
     // declear user's click location.
     float x;
